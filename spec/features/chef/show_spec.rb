9be2d488bc8d2e78dec84ab5020728b6b90b7abe
Daniel <danmoran.pro@gmail.com>
2020-02-21 10:21:59 -0700

    cleaned up code

--- spec/features/chef/show_spec.rb
+++ spec/features/chef/show_spec.rb
@@ -27,17 +27,3 @@ describe "visit a chef's show page" do
         expect(page).to have_content(ingredient_2.name)
     end 
 end 
-
-
-
-
-
-
-
-# s a visitor
-# When I visit a chef's show page
-# I see the name of that chef
-# And I see a link to view a list of all ingredients that this chef uses in their dishes
-# When I click on that link
-# I'm taken to a chef's ingredient index page 
-# and I can s
